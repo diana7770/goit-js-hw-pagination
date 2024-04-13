3bcf3fdc4547f1162fc43e7bf9a9b762ed84d52c
diana7770 <20diane09@gmail.com>
2024-04-13 09:06:34 +0300

    hw

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const list = document.querySelector("ul");
-const btn = document.querySelector(".button");
+const listPhoto = document.querySelector(".list__photo");
+const btn = document.querySelector(".download");
 const btnSave = document.querySelector(".save");
+const searchInput = document.querySelector(".search__input");
+const searchBtn = document.querySelector(".search__btn");
 
 let pageCurrent = 1;
-let blockPage = 3;
-const key = `43133786-570f6afe2f69578830eb496c7`;
+let blockPage = 4;
+const key = "43133786-570f6afe2f69578830eb496c7";
 
-function getPosts() {
-  const url = `https://pixabay.com/api/?key=${key}&per_page=${blockPage}&page=${pageCurrent}`;
+function getPictures(searchTerm) {
+  const url = `https://pixabay.com/api/?key=${key}&q=${searchTerm}&per_page=${blockPage}&page=${pageCurrent}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => createEl(data.hits));
@@ -15,20 +17,19 @@ function getPosts() {
 
 function loadPosts() {
   pageCurrent++;
-  getPosts();
+  const searchTerm = searchInput.value;
+  getPictures(searchTerm);
 }
 
-function createEl(hits) {
-  hits.forEach((el) => {
+const createEl = (el) => {
+  el.forEach((item) => {
     const cardEl = document.createElement("li");
-    cardEl.innerHTML = `
-      <li>
-            <img src="${el.previewURL}" alt="el.tags">
-        </li>
-      `;
-    list.appendChild(cardEl);
+    cardEl.innerHTML = `<li>
+      <img src="${item.previewURL}" alt="${item.tags}">
+    </li>`;
+    listPhoto.appendChild(cardEl);
   });
-}
+};
 
 btn.addEventListener("click", loadPosts);
 
@@ -36,4 +37,10 @@ btnSave.addEventListener("click", () => {
   btn.removeEventListener("click", loadPosts);
 });
 
-getPosts();
+searchBtn.addEventListener("click", () => {
+  pageCurrent = 1;
+  listPhoto.innerHTML = "";
+  loadPosts();
+});
+
+getPictures();
